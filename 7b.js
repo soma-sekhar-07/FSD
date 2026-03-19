3389fea53816b71c15da2d69a73201a6bb07d9c8
soma-sekhar-07 <somasekharannavarapu@gmail.com>
2026-03-20 02:19:51 +0530

    Add HTML file demonstrating Window object usage

---
+++ 7b.js
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Window Object Example</title>
+</head>
+
+<body>
+
+<script>
+// Window object properties
+document.write("Window Height: " + window.innerHeight + "<br>");
+document.write("Window Width: " + window.innerWidth + "<br>");
+
+// Window object method
+window.alert("Welcome to JavaScript Window Object");
+</script>
+
+</body>
+</html>
